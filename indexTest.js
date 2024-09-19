fba09dfa4a4d3713d93a98dedd54a4f459fe3c4e
Irene Mutegi <irene@Ms-Echo.>
2024-09-19 22:39:15 +0300

    complete lab

---
+++ indexTest.js
@@ -0,0 +1,6 @@
+const name = "Joe";  // Change to match the expected value
+const height = 34;   // Ensure height is less than 40
+const message = `${name} is ${height} inches tall`;
+
+module.exports = {name, height, message};
+console.log(message);
